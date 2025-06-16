8c7128f906695e835efa31b6f61bf6bc179de65f
Mateo Cisneros <138953180+mateo2408@users.noreply.github.com>
2025-06-16 08:25:47 -0500

    Actualizacion de AboutVieModel

--- ViewModels/AboutViewModel.cs
+++ ViewModels/AboutViewModel.cs
@@ -1,6 +1,21 @@
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+
 namespace Cisneros_Cahueñas_Puga_Notes.ViewModels;
 
-public class AboutViewModel
+internal class AboutViewModel
 {
-    
+    public string Title => AppInfo.Name;
+    public string Version => AppInfo.VersionString;
+    public string MoreInfoUrl => "https://aka.ms/maui";
+    public string Message => "This app is written in XAML and C# with .NET MAUI.";
+    public ICommand ShowMoreInfoCommand { get; }
+
+    public AboutViewModel()
+    {
+        ShowMoreInfoCommand = new AsyncRelayCommand(ShowMoreInfo);
+    }
+
+    async Task ShowMoreInfo() =>
+        await Launcher.Default.OpenAsync(MoreInfoUrl);
 }
